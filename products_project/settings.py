b30cb4804333c20763881b48b5de5b51be0202bf
naveen199201 <naveen199201@gmail.com>
2023-10-30 14:02:54 +0530

    second commit

--- products_project/settings.py
+++ products_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%7#=0a(1+lgzj^%^&wv+j5a((t^s_4z7p)s)x*noj*nteh6!io'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
